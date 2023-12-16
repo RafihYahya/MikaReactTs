3aa5587899f90ee7c063c90537660c364c6eead4
RafihYahya <rafihyahya@gmail.com>
2023-12-16 14:49:16 +0100

    .

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,8 +1,10 @@
 
 const ErrorPage = ():JSX.Element => {
   return (
-    <div className="w-full h-screen m-4">
-      <div className="flex items-center justify-center">ErrorPage</div>
+    <div className="w-full h-[80vh] m-4">
+      <div className="flex items-center justify-center">
+        <h1 className="text-4xl text-red-600">ErrorPage</h1>
+      </div>
     </div>
   )
 }
