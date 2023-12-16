beca5673d39d1f0a828f1b237c273ac074d51273
RafihYahya <rafihyahya@gmail.com>
2023-12-16 14:56:51 +0100

    .

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,10 +1,10 @@
+import {Link} from 'react-router-dom'
 
 const ErrorPage = ():JSX.Element => {
   return (
-    <div className="w-full h-[80vh] m-4">
-      <div className="flex items-center justify-center">
-        <h1 className="text-4xl text-red-600">ErrorPage</h1>
-      </div>
+    <div className="w-full h-[90vh]  flex flex-col items-center justify-center">
+        <h1 className="text-7xl text-violet-500 uppercase">Error</h1>
+        <Link to="/" className='underline text-lg'>Go Back To <span className='text-violet-400'>Home</span></Link>
     </div>
   )
 }
