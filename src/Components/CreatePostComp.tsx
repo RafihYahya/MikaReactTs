f65ee92b45343a581932a7f60fa0e726d5c8dfcf
RafihYahya <rafihyahya@gmail.com>
2023-12-21 18:47:27 +0100

    WorkingOnLayout

--- src/Components/CreatePostComp.tsx
+++ src/Components/CreatePostComp.tsx
@@ -2,8 +2,14 @@
 
 function CreatePostComp() {
   return (
-    <div className="">
-        
+    <div className=" border-t-4 border-pink-600/75 flex items-start justify-between gap-4 mx-auto my-[5vh] bg-[#ede8e807] w-[80%] h-[50vh] rounded-lg py-8 px-8">
+      <div className="h-36 w-36 rounded-full bg-[#ede8e815] translate-x-[-5%] lg:block hidden"></div>
+      <div className="flex items-end justify-between flex-col  h-full gap-2 lg:w-[80%] w-full ">
+        <div className="bg-[#ede8e815] h-[90%] w-full py-10 rounded-lg "></div>
+        <div className="group transition-colors duration-200 ease-in mt-2 border-[3px] cursor-pointer border-pink-800/75 hover:border-pink-600/75 hover:bg-pink-600/40  w-36 h-20 rounded-md translate-y-[15%]  flex items-center justify-center ">
+          <h1 className="transition-colors duration-200 ease-in  uppercase text-lg">Post</h1>
+          </div>
+      </div>
     </div>
   )
 }
--- src/Components/TabsComp.tsx
+++ src/Components/TabsComp.tsx
@@ -1,14 +1,34 @@
-
+import { Fire, Heart, Warning } from "@phosphor-icons/react";
+import { useState } from "react"
 
 function TabsComp() {
+    const [toggleMobile] = useState<boolean>(false);
     return (
-        
-        <div className="flex justify-between items-center h-full w-[80%] m-4 ">
-            <div className=" transition-all duration-300 ease-in border-t-4 border-pink-600 text-center bg-[#EDE8E805]  backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Most Liked</div>
-            <div className=" transition-all duration-300 ease-in text-center bg-[#EDE8E805] hover:bg-pink-600/50 cursor-pointer hover:backdrop-blur-none backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Trending</div>
-            <div className="transition-all duration-300 ease-in    text-center bg-[#EDE8E805] hover:bg-pink-600/50 cursor-pointer hover:backdrop-blur-none backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Controversial</div>
-        </div>
-        
+
+        toggleMobile ? <div className="flex justify-between items-center h-full w-[80%] m-4 ">
+            <div className=" transition-all duration-300 ease-in border-t-4 border-pink-600 text-center bg-[#EDE8E807]  backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Most Liked</div>
+            <div className=" transition-all duration-300 ease-in text-center bg-[#EDE8E807] hover:bg-pink-600/50 cursor-pointer hover:backdrop-blur-none backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Trending</div>
+            <div className="transition-all duration-300 ease-in    text-center bg-[#EDE8E807] hover:bg-pink-600/50 cursor-pointer hover:backdrop-blur-none backdrop-blur-sm py-4 px-[07%] rounded-md text-[#EDE8E8] text-lg">Controversial</div>
+        </div> :
+            <div className="flex justify-center gap-8 items-center h-full w-full m-4 px-8 ">
+                <div className=" group flex items-center  border-t-4 border-pink-600 rounded-md ">
+                    <div className="    text-center  py-4  px-2 rounded-md text-pink-600 text-lg"><Heart size={32}/>
+                    </div>
+                    <h1 className="transition-all duration-300 ease-in  text-center text-pink-600 text-lg ">Most Liked</h1>
+                </div>
+                <div className="cursor-pointer group flex items-center  hover:border-t-4 hover:border-pink-600 rounded-md ">
+                    <div className="    text-center  py-4  px-2 rounded-md text-lg"><Fire size={32}/>
+                    </div>
+                    <h1 className="transition-all duration-300 ease-in  text-center  text-lg hidden ">Trending</h1>
+                </div>
+                <div className="cursor-pointer group flex items-center  hover:border-t-4 hover:border-pink-600 rounded-md ">
+                    <div className="    text-center  py-4  px-2 rounded-md  text-lg"><Warning size={32}/>
+                    </div>
+                    <h1 className="transition-all duration-300 ease-in  text-center  text-lg hidden ">Controversial</h1>
+                </div>
+                
+            </div>
+
     )
 }
 
