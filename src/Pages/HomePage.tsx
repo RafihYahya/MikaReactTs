ca45dfb07a42944521d851d814cdc1cd2b4d76e9
RafihYahya <rafihyahya@gmail.com>
2023-12-23 14:41:04 +0100

    minor_responsiveChanges

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -11,9 +11,9 @@ const HomePage = (): JSX.Element => {
 
     <>
       <Navbar />
-      <div className='fixed overflow-hidden w-full h-full grid grid-cols-11  p-2  gap-2 lg:pt-24 md:pt-20 '>
-        <div id='sideLines' className='  col-span-2 h-[90.5%]  bg-[#ede8e80a] backdrop-blur-md rounded-sm text-center '></div>
-        <div id='mainCenter' className=' flex flex-col col-span-7 gap-4  h-full '>
+      <div className=' pt-24 fixed overflow-hidden w-full h-full md:grid md:grid-cols-11  p-2  gap-2 lg:pt-24 md:pt-20 '>
+        <div id='sideLines' className=' md:block hidden  md:col-span-1 lg:col-span-2  h-[90.5%] bg-transparent lg:bg-[#ede8e80a] backdrop-blur-md rounded-sm text-center '></div>
+        <div id='mainCenter' className=' flex flex-col md:col-span-9 lg:col-span-7 gap-4  h-full '>
           <div className=' h-[10vh] bg-[#EDE8E805] backdrop-blur-sm rounded-md flex items-center justify-center '>
             <TabsComp />
             </div>
@@ -21,7 +21,7 @@ const HomePage = (): JSX.Element => {
             <CreatePostComp />
           </div>
         </div>
-        <div id='sideLines' className='col-span-2 h-[90.5%]  bg-[#ede8e80b] backdrop-blur-md rounded-md text-center'>
+        <div id='sideLines' className='hidden md:block md:invisible lg:visible md:col-span-1 lg:col-span-2 h-[90.5%] bg-transparent lg:bg-[#ede8e80b] backdrop-blur-md rounded-md text-center'>
           <MiniProfileUserCardComp/>
           <MiniProfileUserCardComp/>
           <MiniProfileUserCardComp/>
