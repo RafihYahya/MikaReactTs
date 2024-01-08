c126865648310fc1672005eb5f881a5a68ce46d9
RafihYahya <rafihyahya@gmail.com>
2024-01-08 17:32:59 +0100

    ..

--- src/Components/CreatePostComp.tsx
+++ src/Components/CreatePostComp.tsx
@@ -3,6 +3,7 @@
 function CreatePostComp() {
   return (
     <div className=" border-t-4 border-pink-600/75 flex items-start justify-between gap-4 mx-auto my-[5vh] bg-[#ede8e807] w-[80%] h-[35vh] md:h-[45vh] lg:h-[50vh] rounded-lg py-8 px-8">
+      
       <div className="h-36 w-36 rounded-full bg-[#ede8e815] translate-x-[-5%] lg:block hidden"></div>
       <div className="flex items-end justify-between flex-col  h-full gap-2 lg:w-[80%] w-full ">
         <div className="bg-[#ede8e815] h-[90%] w-full py-10 rounded-md md:rounded-lg "></div>
@@ -10,6 +11,7 @@ function CreatePostComp() {
           <h1 className="transition-colors duration-200 ease-in  uppercase text-lg">Post</h1>
           </div>
       </div>
+      
     </div>
   )
 }
