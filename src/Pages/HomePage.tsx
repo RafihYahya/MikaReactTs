8e190b356ea1fe55dae625fc09349b307c1939ec
RafihYahya <rafihyahya@gmail.com>
2024-01-16 01:51:13 +0100

    ..

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -34,7 +34,7 @@ const HomePage = (): JSX.Element => {
     <>
       <Navbar />
       <div className=' pt-24 fixed overflow-hidden w-full h-full md:grid md:grid-cols-11  p-2  gap-2 lg:pt-24 md:pt-20 '>
-        <div id='sideLines' className='py-4 px-2 md:block hidden  md:col-span-1 lg:col-span-2  h-[90.5%] bg-transparent lg:bg-[#ede8e805] backdrop-blur-md rounded-sm text-center '>
+        <div id='sideLines' className='py-4 px-2 md:block hidden  md:col-span-1 lg:col-span-2 h-[100%] 2xl:h-[95%] bg-transparent lg:bg-[#ede8e805] backdrop-blur-md rounded-sm text-center '>
           <MainGroupSidelineCardComp/>
           <MainGroupSidelineCardComp/>
           <MainGroupSidelineCardComp/>
@@ -47,14 +47,14 @@ const HomePage = (): JSX.Element => {
           <div id="scrollable" className=' h-screen bg-[#EDE8E805] backdrop-blur-md rounded-md text-center overflow-auto  '>
             <CreatePostComp />
             <div className='h-[10vh]'></div>
-            <h1 id="spliter" className='text-left ml-[5%] text-2xl font-light uppercase  text-pink-500/75 tracking-widest	  w-[90%]'>P<span className='text-pink-500/75 tracking-wider'>osts</span></h1>
+            <h1 id="spliter" className='text-left ml-[5%] text-2xl font-light uppercase  text-pink-600/75 tracking-widest	  w-[90%]'>P<span className='text-pink-600/75 tracking-wider'>osts</span></h1>
             <PostComp />
             <PostComp />
             <PostComp />
             <div className='h-[35vh]'></div>
           </div>
         </div>
-        <div id='sideLines' className='hidden md:block md:invisible lg:visible md:col-span-1 lg:col-span-2 h-[90.5%] bg-transparent lg:bg-[#ede8e805] backdrop-blur-md rounded-md text-center'>
+        <div id='sideLines' className='hidden md:block md:invisible lg:visible md:col-span-1 lg:col-span-2 h-[100%] 2xl:h-[90.5%] bg-transparent lg:bg-[#ede8e805] backdrop-blur-md rounded-md text-center'>
           <div className='mt-4'>
             <MiniProfileUserCardComp />
           </div>
