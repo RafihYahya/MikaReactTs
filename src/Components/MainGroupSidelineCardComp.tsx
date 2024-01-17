f00ca68422bcb128561c31b7cd381be2eebcbdc9
RafihYahya <rafihyahya@gmail.com>
2024-01-17 01:48:57 +0100

    ResponsiveFixes

--- src/Components/MainGroupSidelineCardComp.tsx
+++ src/Components/MainGroupSidelineCardComp.tsx
@@ -2,13 +2,13 @@
 const MainGroupSidelineCardComp = () => {
     return (
         <div className="w-full h-40 bg-[#EDE8E805] backdrop-blur-md rounded-md flex justify-center items-start gap-2 p-4 mb-2">
-            <div className="flex justify-center  items-center flex-col gap-2 mr-2">
+            <div className="flex justify-center  items-center flex-col 2xl:gap-2 gap-3 mr-2">
                 <div className="w-24 h-24 rounded-full bg-[#EDE8E815]  "></div>
                 <div className="text-sm font-light ">Miaw Group</div>
             </div>
-            <div className="flex justify-between items-center flex-col text-sm h-full gap-2 py-2">
-                <p className="text-xs text-left p-2 ">Lorem ipsum dolor sit amet consectetur,Lorem ipsum dolor sit amet consectetur,</p>
-                <h3 className="text-pink-400">X New Posts</h3>
+            <div className="flex justify-between items-center flex-col text-sm h-full gap-2 2xl:py-2 ">
+                <p className="text-xs text-left p-2 font-light">Lorem ipsum dolor sit amet consectetur,Lorem ipsum dolor sit amet consectetur,</p>
+                <h3 className="text-pink-400 2xl:translate-y-[3px]">X New Posts</h3>
             </div>
         </div>
     )
--- src/Components/MiniProfileUserCardComp.tsx
+++ src/Components/MiniProfileUserCardComp.tsx
@@ -2,7 +2,7 @@ import { ChatsCircle } from "@phosphor-icons/react"
 
 const MiniProfileUserCardComp = () => {
   return (
-    <div className="flex justify-start items-center h-20 rounded-md  mb-1 pl-6 bg-[#EDE8E805]  ml-2 mr-3   backdrop-blur-sm gap-10">
+    <div className="flex justify-start items-center h-20 rounded-md  mb-1 2xl:pl-6 pl-2 bg-[#EDE8E805]  ml-2 mr-3   backdrop-blur-sm gap-10">
 
       <div className="w-16 h-16 bg-[#EDE8E810] backdrop-blur-sm rounded-full border-l-2 border-r-2 border-pink-500/30"></div>
       <div>
